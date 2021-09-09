123ac409caf1656916eddebe3b0c037d232bc665
Marcin Krzysztoszek <marcinkrzysztoszek92@gmail.com>
2021-09-09 20:41:11 +0200

    dodany lighbox w galerii, zmiana sekcji, edytowany kontakt

--- include/main.js
+++ include/main.js
@@ -1,8 +1,6 @@
 btns = [...document.querySelectorAll('ul li')];
 sections = [...document.querySelectorAll('section')]
 
-
-
 for (let i = 0; btns.length > i; i++ ) {
     btns[i].addEventListener('click', function(){
         showItem = btns[i].dataset.section;
@@ -19,3 +17,19 @@ for (let i = 0; btns.length > i; i++ ) {
         }
     })
 }
+
+document.querySelector('header img').addEventListener('click', ()=>{
+    console.log('klik')
+    for (let i = 0; sections.length > i; i++ ){
+        if ( i === 0) {
+            sections[i].classList.remove('off')
+        } else {
+            sections[i].classList.add('off')
+        }
+    }
+})
+
+lightbox.option({
+    'resizeDuration': 150,
+    'wrapAround': true
+  })
