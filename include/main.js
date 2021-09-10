4f193a5ec738cd22b2c2ae30d8851341e280763b
Marcin Krzysztoszek <marcinkrzysztoszek92@gmail.com>
2021-09-10 18:53:14 +0200

    dodane animacje, dostosowane okienka na tekst, body bgcImg, funkcja do randomowego Li efektu

--- include/main.js
+++ include/main.js
@@ -1,17 +1,14 @@
 btns = [...document.querySelectorAll('ul li')];
-sections = [...document.querySelectorAll('section')]
+sections = [...document.querySelectorAll('section')];
 
 for (let i = 0; btns.length > i; i++ ) {
     btns[i].addEventListener('click', function(){
         showItem = btns[i].dataset.section;
         for (let i = 0; sections.length > i; i++ ){
-            // jeżeli klasa zawiera nazwę taką jak dataset
             if ( sections[i].classList.contains(showItem) ) {
-                // schowaj wszystkie
                 for (let i = 0; sections.length > i; i++ ){
                     sections[i].classList.add('off')
                 }
-                // włącz wybrany
                 sections[i].classList.remove('off')
             }
         }
@@ -19,7 +16,6 @@ for (let i = 0; btns.length > i; i++ ) {
 }
 
 document.querySelector('header img').addEventListener('click', ()=>{
-    console.log('klik')
     for (let i = 0; sections.length > i; i++ ){
         if ( i === 0) {
             sections[i].classList.remove('off')
@@ -28,6 +24,21 @@ document.querySelector('header img').addEventListener('click', ()=>{
         }
     }
 })
+
+lis = [...document.querySelectorAll('li')];
+
+setInterval(()=>{
+    let number = Math.floor(Math.random() * 4)
+    for ( let i = 0; lis.length > i; i++ ) {
+        if ( i === number ) {
+            lis[i].classList.add('feature')
+            setTimeout(()=>{
+                lis[i].classList.remove('feature')
+            }, 600)
+        }
+
+    }
+}, 5000)
 
 lightbox.option({
     'resizeDuration': 150,
